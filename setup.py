d64d42560907f25bb22de41c0f4dc86c1010817d
wiscot <ragutimar@gmail.com>
2023-02-08 23:12:03 +0100

    Set new version package number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='safetypes',
-    version='0.1rc2',
+    version='0.1rc2post1',
     packages=['safetypes', 'safetypes/base'],
     url='https://github.com/wiscot/safetypes',
     license='MIT',
